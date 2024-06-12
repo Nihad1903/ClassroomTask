053af3b725670bbe620d1b5e5b8f48cd12ec8264
Nihad1903 <152027230+Nihad1903@users.noreply.github.com>
2024-06-12 13:37:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Task 1
+/*int number = Convert.ToInt32(Console.ReadLine());
+if (number % 3 == 0 & number % 7 == 0)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}*/
+
+// Task 2
+/*string name = "salam";
+string your_name = Console.ReadLine();
+if (name == your_name)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}*/
+
+// Task 3
+/*int num = Convert.ToInt32(Console.ReadLine());
+int last_digit = num % 10;
+Console.WriteLine(last_digit);*/
